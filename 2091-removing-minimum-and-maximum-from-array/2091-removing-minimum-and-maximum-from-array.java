7d3842ec94bf80fba75ccfd9eb58e50d6afa9f66
lazy_coder <dipeshgoenka07@gmail.com>
2026-08-30 16:18:53 +0530

    Time: 3 ms (61.82%), Space: 86.7 MB (60.18%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
         int min = 0 , max = 0;
-        for(int i=1 ;i<n ;i++){
+        for(int i=0 ;i<n ;i++){
             if(nums[i] < nums[min]){
                 min = i;
             }
