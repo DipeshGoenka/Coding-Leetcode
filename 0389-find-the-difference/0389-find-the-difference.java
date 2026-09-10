348529c9703321102e43f4a7760d1f12f555a826
lazy_coder <dipeshgoenka07@gmail.com>
2026-09-10 22:01:01 +0530

    Time: 1 ms (99.8%), Space: 43.1 MB (71.21%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char a = 0;
+        for(char c : s.toCharArray())
+        a ^= c;
+        for(char c : t.toCharArray())
+        a ^= c;
+        return a;
+    }
+}
